6d4b8137775cc1c53b3018f13e9b8ebc834a1b45
Shane S <gs@shanewilliamscott.com>
2024-05-01 13:05:40 -0500

    Update code, Update actions, remove old dist bits

--- pyHaveIBeenPwned/__init__.py
+++ pyHaveIBeenPwned/__init__.py
@@ -1,71 +1,59 @@
 import json
-import re
 import cfscrape
 
-class pyHaveIBeenPwned():
-    #End Nodes#
-    apiEndpoint = "https://haveibeenpwned.com/api/v2/"
-    errorStrings = {400: "400 - Bad request - Invalid account specified",
-                    403: "403 - Forbidden - Request is forbidden",
-                    404: "404 - Not found - No account match",
-                    429: "429 - Rate limit exceeded - The rate limit for the API has been reached. Please try again later",
-                    "5XX": "5XX - Server error - The server returned an error"}
-                    
-    #Constructor
-    def __init__(self):
-        return
+class PyHaveIBeenPwned:
+    API_ENDPOINT = "https://haveibeenpwned.com/api/v2/"
+    ERROR_STRINGS = {
+        400: "400 - Bad request - Invalid account specified",
+        403: "403 - Forbidden - Request is forbidden",
+        404: "404 - Not found - No account match",
+        429: "429 - Rate limit exceeded - The rate limit for the API has been reached. Please try again later",
+        "5XX": "5XX - Server error - The server returned an error"
+    }
 
     def makeScrapedRequest(self, urlToFetch):
         requestScraper = cfscrape.create_scraper()
-        r = requestScraper.get(url=urlToFetch)
-        if r.status_code >= 500:
-            return self.errorStrings["5XX"]
-        elif r.status_code >= 400 and r.status_code < 500:
-            return self.errorStrings[int(r.status_code)]
+        response = requestScraper.get(url=urlToFetch)
+        if response.status_code >= 500:
+            return self.ERROR_STRINGS["5XX"]
+        elif 400 <= response.status_code < 500:
+            return self.ERROR_STRINGS[response.status_code]
         else:
-            resultContent = r.content
-            resultContent = resultContent.decode('utf8')
-            resultContent = json.loads(resultContent)
-            return resultContent
+            content = response.content.decode('utf8')
+            return json.loads(content)
 
     def getAccountBreaches(self, email, domain=None):
         urlEndpoint = "breachedaccount/"
+        url = f"{self.API_ENDPOINT}{urlEndpoint}{email}"
         if domain:
-            url = "{0}{1}{2}?domain={3}".format(self.apiEndpoint, urlEndpoint, email, domain)
-        else:
-            url = "{0}{1}{2}".format(self.apiEndpoint, urlEndpoint, email)
-        r = self.makeScrapedRequest(url)
-        return r
+            url += f"?domain={domain}"
+        return self.makeScrapedRequest(url)
 
     def getDomainBreaches(self, domain):
         urlEndpoint = "breaches/"
-        url = "{0}{1}?domain={2}".format(self.apiEndpoint, urlEndpoint, domain)
-        r = self.makeScrapedRequest(url)
-        return r
+        url = f"{self.API_ENDPOINT}{urlEndpoint}?domain={domain}"
+        return self.makeScrapedRequest(url)
 
     def getDomainBreach(self, name):
         urlEndpoint = "breach/"
-        url = "{0}{1}{2}".format(self.apiEndpoint, urlEndpoint, name)
-        r = self.makeScrapedRequest(url)
-        return r
+        url = f"{self.API_ENDPOINT}{urlEndpoint}{name}"
+        return self.makeScrapedRequest(url)
 
     def getDataClasses(self):
         urlEndpoint = "dataclasses/"
-        url = "{0}{1}".format(self.apiEndpoint, urlEndpoint)
-        r = self.makeScrapedRequest(url)
-        return r
+        url = f"{self.API_ENDPOINT}{urlEndpoint}"
+        return self.makeScrapedRequest(url)
 
     def getAccountPastes(self, email):
         urlEndpoint = "pasteaccount/"
-        url = "{0}{1}{2}".format(self.apiEndpoint, urlEndpoint, email)
-        r = self.makeScrapedRequest(url)
-        return r
+        url = f"{self.API_ENDPOINT}{urlEndpoint}{email}"
+        return self.makeScrapedRequest(url)
 
     def testMe(self):
-        t = self.getAccountBreaches("foo@bar.com")
-        return t
+        return self.getAccountBreaches("foo@bar.com")
 
 if __name__ == "__main__":
-    testClassInstance = pyHaveIBeenPwned()
-    testResults = testClassInstance.testMe()
-    print(testResults)
+    instance = PyHaveIBeenPwned()
+    results = instance.testMe()
+    print(results)
+
